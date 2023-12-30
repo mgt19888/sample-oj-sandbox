66b44bce17cf08fb42dfce5a72647d5010d6c581
mgt19888 <1152719547@qq.com>
2023-12-30 18:01:09 +0800

    feat: 构建执行运行代码的流程

---
+++ src/main/java/com/mgt/ojcodesandbox/MgtuojCodeSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.mgt.ojcodesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgtuojCodeSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MgtuojCodeSandboxApplication.class, args);
+    }
+
+}
